25719419dda3e83431877ec1abd125f386175cda
Alexis0110 <alexis.raptakis@freenet.de>
2023-05-23 23:55:33 +0200

    first steps

---
+++ CookingFam/CookingFam/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Persistence.swift
+//  CookingFam
+//
+//  Created by Alexis on 11.05.23.
+//
+
+
+
+struct Model {
+    
+}
---
+++ CookingFam/CookingFam/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  CookingFam
+//
+//  Created by Alexis on 23.05.23.
+//
+
+import Foundation
+import SwiftUI
+import LocalAuthentication
+
+extension ContentView {
+    @MainActor class ViewModel:ObservableObject{
+    }
+}
+
+
